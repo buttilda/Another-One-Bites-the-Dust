14611633498fb3dfe075a209a6a5d1fe5556e53c
ganymedes01 <foka_12@hotmail.com>
2014-02-12 22:54:18 +0000

    version 1.2.0

--- common/ganymedes01/aobd/lib/Reference.java
+++ common/ganymedes01/aobd/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "aobd";
 	public static final String MOD_NAME = "Another One Bites The Dust";
-	public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.849,);required-after:TConstruct;after:IC2;after:Mekanism";
-	public static final String VERSION_NUMBER = "1.1.1";
+	public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.849,);required-after:TConstruct@[1.5.3,);after:IC2;after:Mekanism";
+	public static final String VERSION_NUMBER = "1.2.0";
 }
