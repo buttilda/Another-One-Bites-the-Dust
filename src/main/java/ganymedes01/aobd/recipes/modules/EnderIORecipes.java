c07935b10d7e7379ac718ed28cd23f8aa2f27eca
ganymedes01 <foka_12@hotmail.com>
2014-10-09 18:29:33 -0300

    Fix EnderIO integration

--- src/main/java/ganymedes01/aobd/recipes/modules/EnderIORecipes.java
+++ src/main/java/ganymedes01/aobd/recipes/modules/EnderIORecipes.java
@@ -2,18 +2,26 @@ package ganymedes01.aobd.recipes.modules;
 
 import ganymedes01.aobd.ore.Ore;
 import ganymedes01.aobd.recipes.RecipesModule;
-
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class EnderIORecipes extends RecipesModule {
 
 	public static final String[] blacklist = { "iron", "gold", "copper", "tin", "lead", "silver", "nickel" };
+
+	//@formatter:off
+	private static final String xmlMessage ="<recipeGroup name=\"AOBD\">" + 
+												"<recipe name=\"%sOre\" energyCost=\"%f\">" +
+      										 		"<input>" +
+      										 			"<itemStack oreDictionary=\"ore%s\" />" +
+      										 		"</input>" +
+      										 		"<output>" +
+      										 		"<itemStack oreDictionary=\"dust%s\" number=\"2\" />" +      
+        												"<itemStack oreDictionary=\"dust%s\" number=\"1\" chance=\"0.1\" />" +       
+        												"<itemStack modID=\"minecraft\" itemName=\"cobblestone\" chance=\"0.15\"/>" +
+        											"</output>" +
+        										"</recipe>" + 
+        									"</recipeGroup>";
+	//@formatter:on
 
 	public static void init() {
 		label: for (Ore ore : Ore.ores)
@@ -22,29 +30,11 @@ public class EnderIORecipes extends RecipesModule {
 					if (ore.name().equalsIgnoreCase(bEntry))
 						continue label;
 
-				addSAGMillRecipe("ore" + ore.name(), (float) ore.energy(360.0), new ItemStack[] { getOreDictItem("dust" + ore.name(), 2), getOreDictItem("dust" + ore.extra()), new ItemStack(Blocks.cobblestone) }, new float[] { 1.0F, 0.1F, 0.15F });
+				addSAGMillRecipe(ore.name(), (float) ore.energy(360.0), ore.extra());
 			}
 	}
 
-	@SuppressWarnings("all")
-	private static void addSAGMillRecipe(String input, float energy, ItemStack[] outputs, float[] chance) {
-		try {
-			Object SAGMill = Class.forName("crazypants.enderio.machine.crusher.CrusherRecipeManager").getMethod("getInstance").invoke(null);
-			Method addRecipe = SAGMill.getClass().getMethod("addRecipe", Class.forName("crazypants.enderio.machine.recipe.Recipe"));
-
-			Class recipeInput = Class.forName("crazypants.enderio.machine.recipe.RecipeInput");
-			Class recipeOuput = Class.forName("crazypants.enderio.machine.recipe.RecipeOutput");
-			Constructor oreDictInput = Class.forName("crazypants.enderio.machine.recipe.OreDictionaryRecipeInput").getConstructor(ItemStack.class, int.class, int.class);
-
-			Object[] output = (Object[]) Array.newInstance(Class.forName("crazypants.enderio.machine.recipe.RecipeOutput"), outputs.length);
-			Constructor recipe = Class.forName("crazypants.enderio.machine.recipe.Recipe").getConstructor(recipeInput, float.class, output.getClass());
-
-			for (int i = 0; i < outputs.length; i++)
-				output[i] = recipeOuput.getConstructor(ItemStack.class, float.class).newInstance(outputs[i], chance[i]);
-
-			addRecipe.invoke(SAGMill, recipe.newInstance(oreDictInput.newInstance(getOreDictItem(input, 1), OreDictionary.getOreID(input), -1), energy, output));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private static void addSAGMillRecipe(String input, float energy, String extra) {
+		FMLInterModComms.sendMessage("EnderIO", "recipe:sagmill", String.format(xmlMessage, input, energy, input, input, extra));
 	}
 }
