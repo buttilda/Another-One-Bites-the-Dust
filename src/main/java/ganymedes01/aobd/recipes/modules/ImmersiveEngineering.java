2f4515097f4e03cc4f4617ee2f6f89174785e8f2
ganymedes01 <foka_12@hotmail.com>
2015-07-05 21:26:05 -0300

    Blacklist and secondary outputs for IE

--- src/main/java/ganymedes01/aobd/recipes/modules/ImmersiveEngineering.java
+++ src/main/java/ganymedes01/aobd/recipes/modules/ImmersiveEngineering.java
@@ -3,17 +3,23 @@ package ganymedes01.aobd.recipes.modules;
 import ganymedes01.aobd.lib.CompatType;
 import ganymedes01.aobd.ore.Ore;
 import ganymedes01.aobd.recipes.RecipesModule;
+import net.minecraft.item.ItemStack;
 import blusunrize.immersiveengineering.api.CrusherRecipe;
 
 public class ImmersiveEngineering extends RecipesModule {
 
 	public ImmersiveEngineering() {
-		super(CompatType.IMMERSIVE_ENGINEERING);
+		super(CompatType.IMMERSIVE_ENGINEERING, "iron", "gold", "copper", "lead", "silver", "nickel", "aluminum");
 	}
 
 	@Override
 	public void initOre(Ore ore) {
 		CrusherRecipe.addRecipe(getOreStack("dust", ore), "ingot" + ore.name(), 2400);
-		CrusherRecipe.addRecipe(getOreStack("dust", ore, 2), "ore" + ore.name(), 4000);
+		CrusherRecipe recipe = CrusherRecipe.addRecipe(getOreStack("dust", ore, 2), "ore" + ore.name(), 4000);
+		try {
+			ItemStack extra = getOreStack("dust" + ore.extra());
+			recipe.addSecondaryOutput(extra, 0.1F);
+		} catch (IllegalArgumentException e) {
+		}
 	}
 }
