7d950861d59c44f4750d9d18d50c6cec87b3a28b
ganymedes01 <foka_12@hotmail.com>
2016-06-04 16:22:47 -0300

    Bump version

--- src/main/java/ganymedes01/aobd/lib/Reference.java
+++ src/main/java/ganymedes01/aobd/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "aobd";
 	public static final String MOD_NAME = "Another One Bites The Dust";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1558,);after:*";
-	public static final String VERSION_NUMBER = "2.9.1";
+	public static final String VERSION_NUMBER = "2.9.2";
 
 	public static final String GUI_FACTORY_CLASS = "ganymedes01.aobd.configuration.ConfigGuiFactory";
 }
